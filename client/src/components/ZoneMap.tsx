839cd2b8ee6d8701cc22d043c1f2556c28bc54ca
Raine <64136899+raineaeternal@users.noreply.github.com>
2024-11-01 11:56:39 +0100

    Attempting grid of maps

---
+++ client/src/components/ZoneMap.tsx
@@ -0,0 +1,14 @@
+import "./ZoneMap.css";
+
+export function ZoneMap() {
+    return (
+        <>
+            <div className="map">
+                <img src="../assets/Eureka_Anemos.png" alt="Map of Eureka Anemos" />
+                <img src="../assets/Eureka_Pagos.png.png" alt="Map of Eureka Pagos" />
+                <img src="../assets/Eureka_Pyros.png" alt="Map of Eureka Pyros" />
+                <img src="../assets/Eureka_Hydatos.png" alt="Map of Eureka Hydatos" />
+            </div>
+        </>
+    );
+}
