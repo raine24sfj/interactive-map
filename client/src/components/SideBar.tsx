18a12f80714aab6512e04c41fe41f751a240e64c
Raine <64136899+raineaeternal@users.noreply.github.com>
2024-11-04 14:00:22 +0100

    update css to use tailwind components

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,5 +1,3 @@
-import "./styles/SideBar.css"
-
 export function SideBar() {
     return (
         <>
--- client/src/components/ZoneMap.tsx
+++ client/src/components/ZoneMap.tsx
@@ -1,4 +1,3 @@
-import "./styles/ZoneMap.css";
 import anemos from "../assets/Eureka_Anemos.png";
 import pagos from "../assets/Eureka_Pagos.png";
 import pyros from "../assets/Eureka_Pyros.png";
@@ -7,13 +6,11 @@ import hydatos from "../assets/Eureka_Hydatos.png";
 export function ZoneMap() {
     return (
         <>
-            <div className="">
-                <div className="">
-                    <img src={anemos} alt="Map of Eureka Anemos" />
-                    <img src={pagos} alt="Map of Eureka Pagos" />
-                    <img src={pyros} alt="Map of Eureka Pyros" />
-                    <img src={hydatos} alt="Map of Eureka Hydatos" />
-                </div>
+            <div className="grid grid-cols-2">
+                <img src={anemos} alt="Map of Eureka Anemos" />
+                <img src={pagos} alt="Map of Eureka Pagos" />
+                <img src={pyros} alt="Map of Eureka Pyros" />
+                <img src={hydatos} alt="Map of Eureka Hydatos" />
             </div>
         </>
     );
