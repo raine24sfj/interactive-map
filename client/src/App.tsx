f93a79bf1ad9735926441d780f3a4200665f6799
Raine <64136899+raineaeternal@users.noreply.github.com>
2024-11-04 13:54:04 +0100

    update remaining things that were forgotten in the tailwind + sass conversion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import { lazy } from "react"
 import { SideBar } from './components/SideBar';
 import { ZoneMap } from './components/ZoneMap';
@@ -8,7 +8,7 @@ import { ZoneMap } from './components/ZoneMap';
 function App() {
   return (
     <>
-    <div className="justify-around">
+    <div className="bg-[#000] flex-row justify-around items-center font-normal ">
       <SideBar />
       <ZoneMap />
     </div>
